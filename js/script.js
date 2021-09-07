c2fedfeda03d56cae6190767185c4b771a8a61fb
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-07 16:26:15 +0200

    Aggiunta ciclo for con stampa dei dati e prompt

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ var studente = {
 // console.log(studenti['eta']);
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-for (var key in studente){
-    console.log(studente[key]);
-}
+// for (var key in studente){
+//     console.log(studente[key]);
+// }
 
 // Creare un array di oggetti di studenti.
 var studenti = 
@@ -35,7 +35,32 @@ var studenti =
         }
     ]
 
+// Dare la possibilità all’utente, attraverso 3 prompt(), 
+// di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+    var nuoviDati = {
+        'nome': prompt("Inserisci un nome"),
+        'cognome': prompt("Inserisci un cognome"),
+        'eta': parseInt(prompt("Inserisci l'età"))
+    }; 
+    studenti.push(nuoviDati);
+
+
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+document.getElementById("listaStudenti");
 
 
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+for(var i = 0; i < studenti.length; i++){
+
+    listaStudenti.innerHTML += `<li>
+                                <h3>Nome: ${studenti[i]['nome']}</h3>
+                                <h3>Cognome: ${studenti[i]['cognome']}</h3>
+                                </li>`;
+    console.log(studenti[i]);
+}
+
+
+
+
+
+
